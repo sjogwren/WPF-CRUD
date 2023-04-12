289941907933807c841efe7392d2ab41ac1cc451
Zain Nabi <zainolnabi88@gmail.com>
2023-04-12 04:02:59 +0200

    changed data type
    changed data type

--- WpfAppDemoCRUD/Data/Product.cs
+++ WpfAppDemoCRUD/Data/Product.cs
@@ -13,7 +13,7 @@ namespace WpfAppDemoCRUD.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public int Unit { get; set; }
     }
 }
